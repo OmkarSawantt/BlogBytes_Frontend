63e41bd5e5c5ddb8938ae7ba33efa7be56ed6a48
OmkarSawantt <162661858+OmkarSawantt@users.noreply.github.com>
2024-07-21 18:18:50 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState ,useContext } from 'react'
 import {Link} from "react-router-dom"
-import Logo from '../Images/logo1.png'
+import Logo from '.../public/logo.png'
 import {FaBars} from 'react-icons/fa'
 import {AiOutlineClose} from 'react-icons/ai'
 import {UserContext} from '../context/userContext'
